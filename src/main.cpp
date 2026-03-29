0588d3cb829582c97787a6e786329e5bcbc385c3
Pianetti <samuelpc2006@gmail.com>
2026-03-29 00:54:50 -0300

    Implementação do arquivo output.dat

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main() {
     double valorA, valorB;
 
     // Abre o arquivo para leitura
-    ifstream meuArquivo("/home/samuel/TRABALHOS/AEDS1 - Trabalho de aquecimento/input/Teste.txt");
+    ifstream meuArquivo("/home/samuel/TRABALHOS/AEDS1 - Trabalho de aquecimento/input/input.dat");
 
     // Verifica se o arquivo abriu corretamente
     if (meuArquivo.is_open()) {
@@ -45,7 +45,7 @@ int main() {
     float matriz_coordenadas[N][2];
 
     // Reabre o arquivo
-    meuArquivo.open("/home/samuel/TRABALHOS/AEDS1 - Trabalho de aquecimento/input/Teste.txt");
+    meuArquivo.open("/home/samuel/TRABALHOS/AEDS1 - Trabalho de aquecimento/input/input.dat");
     // Verifica se o arquivo abriu corretamente
     if (meuArquivo.is_open()) {
 
@@ -77,7 +77,6 @@ int main() {
         cout << "Valor A: " << valorA << " Valor B: " << valorB << endl;
     }
     
-    // Calcular erro e fitness de cada indivíduo
     for (int j=0; j<M; j++){
         individuo[j].erro = 0;
         for(int i=0; i<N; i++){
@@ -92,8 +91,8 @@ int main() {
 
     //Ordenar o vetor do fitness em ordem decrescente
     for(int j = 0; j < M; j++){
-            for(int i = 0; i < M - 1; i++){
-                if(individuo[i].fitness < individuo[i+1].fitness){
+        for(int i = 0; i < M - 1; i++){
+            if(individuo[i].fitness < individuo[i+1].fitness){
                 Individuo aux = individuo[i];
                 individuo[i] = individuo[i+1];
                 individuo[i+1] = aux;
@@ -101,62 +100,87 @@ int main() {
         }
     }
 
-    for(int j = 0; j < M; j++){
-        cout << "Fitness do individuo " << j << ": " << individuo[j].fitness << endl;
-    }
+    //Arquivo do output
+    ofstream Arquivo_de_saida("/home/samuel/TRABALHOS/AEDS1 - Trabalho de aquecimento/output/output.dat");
+    if(Arquivo_de_saida.is_open()){
+
+        Arquivo_de_saida << "Número da geração atual:  0" << endl;
+        Arquivo_de_saida << "Valor de a:              " << individuo[0].a << endl;
+        Arquivo_de_saida << "Valor de b:              " << individuo[0].b << endl;
+        Arquivo_de_saida << "Valor do erro:           " << individuo[0].erro << endl;
+        Arquivo_de_saida << "Valor do fitness:        " << individuo[0].fitness << endl << endl;
 
 
+        // Seleção e crossover
+        for(int geracoes = 0; geracoes < G; geracoes++){
 
+            cout << "Geração " << geracoes + 1 << ": " << endl;
 
+            for (int j=0; j<M; j++){
+                individuo[j].erro = 0;
+                for(int i=0; i<N; i++){
+                    individuo[j].erro += pow((matriz_coordenadas[i][1] - (matriz_coordenadas[i][0] * individuo[j].a + individuo[j].b)), 2);
+                }
+                individuo[j].erro = individuo[j].erro / N;
+                individuo[j].fitness = 10000 / (1 + individuo[j].erro);
 
+                cout << "Valor do Fitness do indivíduo " << j << ":    " << individuo[j].fitness << endl;
 
-    // Seleção e crossover
-    for(int geracoes = 0; geracoes < G; geracoes++){
-        
-        cout << "Gereção " << geracoes + 1 << ": " << endl;
-
-        if(geracoes % 2 == 0){
-            individuo[M - 1].a = individuo[0].a;
-            individuo[M - 1].b = individuo[1].b;
-            individuo[M - 2].a = individuo[1].a;
-            individuo[M - 2].b = individuo[0].b;
-        } else {
-
-            // Mutação com uma variação aleatória nos 2 piores
-            uniform_real_distribution<> mutacao(-10.0, 10.0);
-
-            // Um seed diferente em cada geração
-            mt19937 probabilidade(geracoes);
-            individuo[M - 1].a += mutacao(probabilidade);
-            individuo[M - 1].b += mutacao(probabilidade);
-            individuo[M - 2].a += mutacao(probabilidade);
-            individuo[M - 2].b += mutacao(probabilidade);
-        }
-
-        for (int j=0; j<M; j++){
-            individuo[j].erro = 0;
-            for(int i=0; i<N; i++){
-                individuo[j].erro += pow((matriz_coordenadas[i][1] - (matriz_coordenadas[i][0] * individuo[j].a + individuo[j].b)), 2);
             }
-            individuo[j].erro = individuo[j].erro / N;
-            individuo[j].fitness = 10000 / (1 + individuo[j].erro);
 
-            cout << "Valor do Fitness do indivíduo " << j << ":    " << individuo[j].fitness << endl;
-
-        }
-
-        //Ordenar o vetor do fitness em ordem decrescente
-        for(int j = 0; j < M; j++){
-            for(int i = 0; i < M - 1; i++){
-                if(individuo[i].fitness < individuo[i+1].fitness){
-                    Individuo aux = individuo[i];
-                    individuo[i] = individuo[i+1];
-                    individuo[i+1] = aux;
+            //Ordenar o vetor do fitness em ordem decrescente
+            for(int j = 0; j < M; j++){
+                for(int i = 0; i < M - 1; i++){
+                    if(individuo[i].fitness < individuo[i+1].fitness){
+                        Individuo aux = individuo[i];
+                        individuo[i] = individuo[i+1];
+                        individuo[i+1] = aux;
+                    }
                 }
             }
+            
+
+            //Geração par: crossover
+            if(geracoes % 2 == 0){
+
+                //Escolhe alguém da primeira metade aleatoriamente para fazer o crossover com a metade ruim
+                uniform_int_distribution<> crossover(0, M/2 -1);
+                mt19937 chance(geracoes);
+
+                for(int i = M/2; i < M; i++){
+                    double pai_a = crossover(chance);
+                    double pai_b = crossover(chance);
+                    
+                    while(pai_b == pai_a){
+                        pai_b = crossover(chance);
+                    }
+                    individuo[i].a = individuo[pai_a].a;
+                    individuo[i].b = individuo[pai_b].b;
+                }
+
+            //Geração ímpar: Mutação
+            } else {
+                // Mutação com uma variação aleatória nos 2 piores
+                uniform_real_distribution<> mutacao(-0.5, 0.5);
+
+                // Um seed diferente em cada geração
+                mt19937 probabilidade(geracoes);
+                
+                //A metade pior sofre mutação
+                for(int i = M/2; i < M; i++){
+                    individuo[i].a += mutacao(probabilidade);
+                    individuo[i].b += mutacao(probabilidade);
+                }
+            }
+        Arquivo_de_saida << "Número da geração atual: " << geracoes + 1 << endl;
+        Arquivo_de_saida << "Valor de a:              " << individuo[0].a << endl;
+        Arquivo_de_saida << "Valor de b:              " << individuo[0].b << endl;
+        Arquivo_de_saida << "Valor do erro:           " << individuo[0].erro << endl;
+        Arquivo_de_saida << "Valor do fitness:        " << individuo[0].fitness << endl << endl;
         }
-
-
+        Arquivo_de_saida.close();
+    } else{
+        cout << "Erro2" << endl;
     }
 
     return 0;
