5d03753eda4743bdbdcff64090a43bd9472b2e73
Pianetti <samuelpc2006@gmail.com>
2026-03-28 22:22:37 -0300

    Selecioanando, mutando, crossover e reordenando os 2 últimos

--- src/main.cpp
+++ src/main.cpp
@@ -79,6 +79,7 @@ int main() {
     
     // Calcular erro e fitness de cada indivíduo
     for (int j=0; j<M; j++){
+        individuo[j].erro = 0;
         for(int i=0; i<N; i++){
             individuo[j].erro += pow((matriz_coordenadas[i][1] - (matriz_coordenadas[i][0] * individuo[j].a + individuo[j].b)), 2);
         }
@@ -89,14 +90,13 @@ int main() {
 
     }
 
-    //Ordenar o vetor do fitness em ordem crescente
+    //Ordenar o vetor do fitness em ordem decrescente
     for(int j = 0; j < M; j++){
-        for(int i = 0; i < M; i++){
-            if(individuo[i].fitness < individuo[i+1].fitness){
-                float aux = 0;
-                aux = individuo[i].fitness;
-                individuo[i].fitness = individuo[i+1].fitness;
-                individuo[i+1].fitness = aux;
+            for(int i = 0; i < M - 1; i++){
+                if(individuo[i].fitness < individuo[i+1].fitness){
+                Individuo aux = individuo[i];
+                individuo[i] = individuo[i+1];
+                individuo[i+1] = aux;
             }
         }
     }
@@ -105,8 +105,59 @@ int main() {
         cout << "Fitness do individuo " << j << ": " << individuo[j].fitness << endl;
     }
 
-    // Geerações
-    
+
+
+
+
+
+    // Seleção e crossover
+    for(int geracoes = 0; geracoes < G; geracoes++){
+        
+        cout << "Gereção " << geracoes + 1 << ": " << endl;
+
+        if(geracoes % 2 == 0){
+            individuo[M - 1].a = individuo[0].a;
+            individuo[M - 1].b = individuo[1].b;
+            individuo[M - 2].a = individuo[1].a;
+            individuo[M - 2].b = individuo[0].b;
+        } else {
+
+            // Mutação com uma variação aleatória nos 2 piores
+            uniform_real_distribution<> mutacao(-10.0, 10.0);
+
+            // Um seed diferente em cada geração
+            mt19937 probabilidade(geracoes);
+            individuo[M - 1].a += mutacao(probabilidade);
+            individuo[M - 1].b += mutacao(probabilidade);
+            individuo[M - 2].a += mutacao(probabilidade);
+            individuo[M - 2].b += mutacao(probabilidade);
+        }
+
+        for (int j=0; j<M; j++){
+            individuo[j].erro = 0;
+            for(int i=0; i<N; i++){
+                individuo[j].erro += pow((matriz_coordenadas[i][1] - (matriz_coordenadas[i][0] * individuo[j].a + individuo[j].b)), 2);
+            }
+            individuo[j].erro = individuo[j].erro / N;
+            individuo[j].fitness = 10000 / (1 + individuo[j].erro);
+
+            cout << "Valor do Fitness do indivíduo " << j << ":    " << individuo[j].fitness << endl;
+
+        }
+
+        //Ordenar o vetor do fitness em ordem decrescente
+        for(int j = 0; j < M; j++){
+            for(int i = 0; i < M - 1; i++){
+                if(individuo[i].fitness < individuo[i+1].fitness){
+                    Individuo aux = individuo[i];
+                    individuo[i] = individuo[i+1];
+                    individuo[i+1] = aux;
+                }
+            }
+        }
+
+
+    }
 
     return 0;
 }
